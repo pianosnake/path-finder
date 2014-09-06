e22e089f40d90bd0cc9293f5cd899395980c1738
Florin Alexandrescu <pianosnake@gmail.com>
2014-09-06 17:08:14 -0600

    Add A* as an angular service

--- app.js
+++ app.js
@@ -1,93 +1,125 @@
-(function(){
+angular.module('pathFinder', ['astar'])
 
-var app = angular.module('path-finder', []);
-
-app.filter('pointDisplay', function(){
+.filter('pointDisplay', function(){
   return function(pt){
     if(pt){
-      return "x: "+pt.x + " y: "+pt.y+ " v: "+pt.v;
+      return "x: " + pt.x + " y: " + pt.y + " v: " + pt.v;
     }else{
       return "";
     }
   }
-});
+})
 
- app.directive('pathFinderCanvas', function() {
-  var imageData, el, width, height; 
+.directive('pathFinderCanvas', ['astarService', function(astar){
+  var imageData, el, width, height, context, contextImageData;
 
   var getPosition = function(event){
-    var x,y,v;
-    if (event.x && event.y){
-        x = event.x;
-        y = event.y;
+    var x, y, v;
+    if(event.x && event.y){
+      x = event.x;
+      y = event.y;
     }else{
-    // Firefox method to get the position
+      // Firefox
       x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
       y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
     }
     x -= el.offsetLeft;
     y -= el.offsetTop;
-    if(x>width || y>height){
-        //do nothing if the mouse is outside the canvas
-        return; 
-    }
-    v = imageData[4*(x+width*y)];
-    return {x:x,y:y,v:v};
+    v = imageData[4 * (x + width * y)];
+    return {x:x, y:y, v:v};
   };
 
-  return {
-    restrict: 'E',
-    replace: true,
-    template: '<canvas width="{{width}}" height="{{height}}"></canvas>',
-    controller: 'canvasCtrl',
+  var drawCircle = function(x, y, color){
+    var radius = 5;
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = color;
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = '#003300';
+    context.stroke();
+  };
 
-    link: function(scope, elem, attrs) {
-      var context = elem[0].getContext('2d');
+  var drawPath = function(path){
+    context.strokeStyle = "red";
+    context.beginPath();
+    context.moveTo(path[0][0], path[0][1]);
+    for(i=1; i<path.length; i++){
+      context.lineTo(path[i][0], path[i][1]);
+    }
+    context.stroke();
+  }
+
+  var resetCanvas = function(){
+    context.putImageData(contextImageData, 0, 0);
+  }
+
+  return {
+    restrict:'E',
+    replace:true,
+    template:'<canvas></canvas>',
+    controller:'canvasCtrl',
+
+    link:function(scope, elem, attrs){
+      context = elem[0].getContext('2d');
       var img = new Image();
+
       //define an image load event first
       img.onload = function(){
-          context.drawImage(img,0,0);
-          width = attrs.width;
-          height = attrs.height;
-          el = elem[0];
-          var contextImageData = context.getImageData(0,0,width,height);
-          imageData = contextImageData.data;
-        }
-      //then load the image from src param
+        width = img.width;
+        height = img.height;
+        elem[0].setAttribute("width", width);
+        elem[0].setAttribute("height", height);
+        context.drawImage(img, 0, 0);
+        el = elem[0];
+
+        //save the image data for quick lookup
+        contextImageData = context.getImageData(0, 0, width, height)
+        imageData = contextImageData.data;
+      }
+      //load the image from src param
       img.src = attrs.src;
 
-      elem.bind('mousemove', function(event) {
+
+      elem.bind('mousemove', function(event){
         scope.mouseMovePosition = getPosition(event);
         scope.$digest();
       });
 
 
       elem.bind('mousedown', function(event){
-          var pt= getPosition(event);
-          var p = pt.x+"-"+pt.y;
-          
-          if(!scope.startPt){
-              //define the start point.   g is the score to get to itself, its 0 in the beginning, f is a high score as a default start  
-              scope.startPt ={x:pt.x, y:pt.y, v:pt.v,p:p,g:0,f:0,par:null,o:1,i:null};
-          }else{
-              scope.endPt ={x:pt.x,y:pt.y,v:pt.v,p:p};
-          }
-          scope.$digest();
+        var pt = getPosition(event);
+        var p = pt.x + "-" + pt.y;
+
+        if(scope.startPt && scope.endPt){
+          resetCanvas();
+          scope.startPt = null;
+          scope.endPt = null;
+        }
+
+        if(!scope.startPt){
+          //define the start point.   g is the score to get to itself, its 0 in the beginning, f is a high score as a default start
+          scope.startPt = {x:pt.x, y:pt.y, v:pt.v, p:p, g:0, f:0, par:null, o:1, i:null};
+          drawCircle(pt.x, pt.y, 'lightgreen');
+        }else if(!scope.endPt){
+          scope.endPt = {x:pt.x, y:pt.y, v:pt.v, p:p};
+          drawCircle(pt.x, pt.y, 'red');
+
+          setTimeout(function(){
+            var path = astar.solve(scope.startPt, scope.endPt, imageData, width, height);
+            drawPath(path);
+          },10);
+        }
+        scope.$digest();
 
       });
     }
 
   };
-});
+}])
 
- app.controller('canvasCtrl', function($scope){
-    
- })
+.controller('canvasCtrl', function($scope){
 
-
-
-
-})();
-
+})
 
 
